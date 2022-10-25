f5db11cf223db0663375c140d1964bf8e746611f
Lukasz Gintowt <syzer3@gmail.com>
2022-10-25 10:04:02 +0200

    show newErrors for today

---
+++ newErrors.js
@@ -0,0 +1,159 @@
+import {
+  length,
+  prop,
+  values,
+  pipe as _,
+  tap,
+  flatten,
+  map,
+  keys,
+  count,
+  uniqBy,
+  join,
+  head,
+  tryCatch,
+  concat, innerJoin, pluck, groupBy, filter, reject, evolve, pick, slice, propOr, __, always,
+} from 'ramda'
+import { createRequire } from "module";
+import { getToday } from './db.js'
+import { msgToCategory } from './lib/index.js'
+import { formatDistance } from 'date-fns'
+
+const require = createRequire(import.meta.url); // construct the require method
+const data = require("./data/db.json") // use the require method
+
+if (!['prod', 'stage'].includes(process.argv[2]) ) {
+  throw 'Missing argument prod|stage'
+}
+
+const env = process.argv[2] // prod | stage
+
+const recentDays = _(
+  prop(env),
+  e => {
+    const f = { ...e }
+    delete f[getToday()]
+    return f
+  },
+  values,
+  map(values),
+  flatten,
+  // tap(_(length, console.log)),
+  // tap(_(head, console.log)),
+)(data)
+
+const tapFirst = tap(_(
+  head,
+  e => console.log(e)
+))
+
+const tapProp = property => tap(_(
+  prop(property),
+  tap(() => console.log(property, ':')),
+  console.log))
+
+console.log('errors to compare on env:', env, recentDays.length, recentDays.find(e => !e.message))
+
+const todayErr = _(
+  prop(env),
+  prop(getToday()),
+  values)(data)
+
+const newErrors = _(
+  prop(env),
+  prop(getToday()),
+  values,
+  tap(_(
+    length,
+    errs => console.log('Today errs today so far on ', env, errs))), // erro today
+  concat(recentDays),
+
+  uniqBy(msgToCategory), // TODO union unique, or intersect
+  // tap(_(e => pluck('message', e), console.log)),
+  allErr => innerJoin(
+    (record, message) =>
+      msgToCategory(record) === msgToCategory({ message }),
+    todayErr,
+    pluck('message', allErr)
+  ), // Maybe show each error and match?
+  tapFirst,
+  length,
+  // tap(console.log)
+)(data)
+
+// console.log('newErrors from today', newErrors)
+
+const matches = _(
+  groupBy(msgToCategory), // TODO better with nn
+  map(map(_(
+    pick(['time', 'message']), // disable if need to match by category
+    evolve({ time: e => new Date(e) })
+  ))),
+  // map(length)
+  filter(e => e.length > 1),
+)(recentDays)
+
+// console.log(matches)
+
+// Uncatagloged errors
+_(
+  // slice(100, 100),
+  // filter(e => e.message.includes('CannotConnectError')),
+  // tap(_(
+  //   head,
+  //   msgToCategory,
+  //   console.log,
+  //     () => console.log(matches['Redis::CannotConnectError'])
+  //   )),
+  reject(e => matches[msgToCategory(e)]),
+  tap(_(
+    length, e =>
+    console.log('Uncataloged errors', e))),
+  map(
+    evolve({
+      time: e => new Date(e)
+    })),
+  groupBy(msgToCategory),
+  tap(_(map(length), console.log)),
+  // console.log
+  // tapProp('TODO:DavidErrorWillFix')
+  // tapProp('1 renew failure(s), 0 parse failure(s)'),
+  // tapProp('1 renew failure(s), 0 parse failure(s)'),
+)(todayErr)
+
+// Returning errors
+_(
+  // slice(100, 100),
+  // filter(e => e.message.includes('CannotConnectError')),
+  // tap(_(
+  //   head,
+  //   msgToCategory,
+  //   console.log,
+  //     () => console.log(matches['Redis::CannotConnectError'])
+  //   )),
+  filter(e => matches[msgToCategory(e)]),
+  tap(_(length, e => console.log('Cataloged errors', e))),
+  map(
+    evolve({
+      time: e => new Date(e)
+    })),
+  groupBy(msgToCategory),
+  map(
+    _(
+      map(e => {
+        e.firstSeen = formatDistance(
+          // new Date('2020-12-04T15:39:40.000Z'),
+          matches[msgToCategory(e)][0].time,
+          new Date(),
+          { addSuffix: true }
+        )
+        return e
+      }),
+      // pick(['time', 'message', 'firstSeen', 'path'])
+    )),
+  // tapProp('TODO:DavidErrorWillFix'),
+  // tapProp('ActiveRecord::RecordNotFound: ActiveRecord::RecordNotFound'),
+  tapProp('payments'),
+  // tapProp('RemoteServiceUnavailable'),
+  e => console.log('Returning errors categories', map(length, e)),
+)(todayErr)
--- readme.md
+++ readme.md
@@ -1,6 +1,28 @@
-# 
+# WAT
+plugin on to of our dashboard to show what are new errors
+and show we care or not
 
 chrome://extensions/ -> Developer mode -> Load unpacked extension -> select the folder
 
 also inspect console
 and service worker console
+
+# TODO
+- [X] low db
+- maybe localforage would be a better frontend option?
+
+- [X] store every day error 
+- [X] counts and 
+- [X] error messages
+- [X] add staging
+- [X] show new messages,
+    - [X] and last seen
+
+- [ ] dotenv
+   cookie
+
+
+- [ ] add resolutions and links
+- [ ] on logs add button to hide by message(id)
+
+neural net to categorise should I care and highlight
